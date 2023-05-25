56f2af3b530bcf0de438c28ff60a1c2eeb40eab4
Ricardo Klaus <ricardo.klaus@icloud.com>
2023-05-25 15:07:58 -0300

    Done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
 
-  validates :movie_id, uniqueness: { scope: :list_id, message: "has already been bookmarked in this list." }
-  validates :comment, length: { minimum: 6 }, allow_blank: true
+  validates :movie_id, uniqueness: { scope: :list_id }
+  validates :comment, length: { minimum: 6 }
 end
