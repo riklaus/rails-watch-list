7ccb9fb7cbc520514b850b156d9c2be6926f3c27
Ricardo Klaus <ricardo.klaus@icloud.com>
2023-05-26 11:38:06 -0300

    rails new

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,18 +1,6 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, dependent: :destroy
-  has_many :list, through: :bookmarks
+  has_many :bookmarks
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
-
-  before_destroy :check_for_bookmarks
-
-  private
-
-  def check_for_bookmarks
-    if bookmarks.any?
-      errors.add(:base, 'Cannot delete the movie as it is referenced in at least one bookmark.')
-      throw(:abort)
-    end
-  end
 end
